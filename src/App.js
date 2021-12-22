160887e1299c3ea6636cda6e8b221afb1a42e846
JenabB <birulangit1996@gmail.com>
2021-12-22 19:59:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Profile from "./PortfolioContainer/Home/Profile";
+// import TodoList from "./TodoList/TodoList";
+import { Todo } from "./TodoList/index";
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TodoList/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Typography from "@material-ui/core/Typography";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import useTodoState from "./useTodoState";
+import "./styles.css";
+
+export const Todo = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+  console.log(todos);
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+
+      <TodoForm
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      {todos.length > 0 ? (
+        <TodoList todos={todos} deleteTodo={deleteTodo} />
+      ) : (
+        <h1> no todos</h1>
+      )}
+    </div>
+  );
+};
